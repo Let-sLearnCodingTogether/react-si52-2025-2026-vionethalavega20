2189a89628459c69705e0ce1343ede2a60f2933c
vionethalavega20 <vionetha2085@gmail.com>
2025-11-15 18:33:26 +0700

    integrate axios and update Movies page to use ApiClient

--- src/pages/movies/Movies.tsx
+++ src/pages/movies/Movies.tsx
@@ -1,9 +1,36 @@
-import { NavLink } from "react-router"
+import { useCallback, useEffect, useState } from "react";
+import { NavLink } from "react-router";
+import ApiClient from "../../utils/ApiClient";
+
+interface Movie {
+    _id : string,
+    judul : string,
+    tahunRilis : string,
+    sutradara : string,
+    createdAt : string,
+    updatedAt : string
+}
 
 function Movies(){
-    return <div>
-        <h2>Movie Page</h2>
-        <NavLink to ="/add-movie" className="btn btn-primary">Add Movie</NavLink>
+    const [movies, setMovies] = useState<Movie[]>([])
+
+    const fetchMovies = useCallback(async () => {
+        const response = await ApiClient.get("/movie")
+        
+        if(response.status == 200){
+            setMovies(response.data.data)
+        }
+    }, [])
+
+    useEffect(() => {
+        fetchMovies()
+    }, [fetchMovies])
+
+    return <div className = "container mx-auto">
+        <div className = "d-flex justify-content-between mb-3">
+            <h4>Movie Page</h4>
+            <NavLink to ="/add-movie" className="btn btn-primary">Add Movie</NavLink>
+        </div>
     </div>
 }
 
